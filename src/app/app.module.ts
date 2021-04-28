3ab13544cdfa5c05fce0cbd2193d57a553afd19e
Gustavo Henrique <avoguga@gmail.com>
2021-04-28 00:16:19 -0300

    putting some charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,35 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+
+import { ChartsModule } from 'ng2-charts';
+
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { StatsGraphComponent } from './components/graphics/stats-graph/stats-graph.component';
+import { TrafficComponent } from './components/graphics/traffic/traffic.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    StatsGraphComponent,
+    TrafficComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/graphics/traffic/traffic.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType, ChartOptions } from 'chart.js';
+import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
+
+@Component({
+  selector: 'app-traffic',
+  templateUrl: './traffic.component.html',
+  styleUrls: ['./traffic.component.scss']
+})
+export class TrafficComponent implements OnInit {
+
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartData: SingleDataSet = [300, 500, 100];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+  constructor() {
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
+  }
+  ngOnInit(): void {
+  }
+
+}
